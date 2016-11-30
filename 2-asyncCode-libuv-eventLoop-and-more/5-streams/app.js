ab485391fdc0fd634fddacabcaca5c294f963fec
Brian Doane <brianmdoane@gmail.com>
2016-11-30 10:54:27 -0600

    core of readble and writable streams

--- 2-asyncCode-libuv-eventLoop-and-more/5-streams/app.js
+++ 2-asyncCode-libuv-eventLoop-and-more/5-streams/app.js
@@ -2,11 +2,14 @@
 
 const { createReadStream, createWriteStream } = require('fs')
 
-// Encoding will return it as readable characters, highWaterMark will determine the number of bytes you want the buffer to be
+// Encoding will return it as readable characters, highWaterMark will determine the number of bytes you want the buffer chunks to be
 let readable = createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 })
 
+let writable = createWriteStream(__dirname + '/greetcopy.txt')
 // Since this inherits from Stream which inherits from EventEmitter we can use on
 readable.on('data', function(chunk) {
 	// To see chunk bits
 	console.log(chunk.length)
+	console.log(chunk)
+	writable.write(chunk)
 })
