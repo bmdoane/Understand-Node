60e17fc39f7387a23323f06dcee7d6378c7bc24e
Brian Doane <brianmdoane@gmail.com>
2016-12-02 15:39:24 -0600

    added url tracker via middleware

--- 4-express/app.js
+++ 4-express/app.js
@@ -5,8 +5,17 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-// Middleware
+
+// MIDDLEWARE
+// Any files in public folder become available to routes with /assets 
 app.use('/assets', express.static(__dirname + '/public'))
+// Log url in console with each page change
+app.use('/', (req, res, next) => {
+	console.log(`Request URL: ${req.url}`)
+	// Call the next middleware connected to this route
+	next()
+})
+
 
 // Responds to HTTP request with method get and map to URL '/' 
 app.get('/', (req, res) => {
