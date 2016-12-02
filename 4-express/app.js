2213d9b706cd36e79d570615cf2ba0fd4fef6542
Brian Doane <brianmdoane@gmail.com>
2016-12-02 14:50:37 -0600

    added stylesheet styles via middleware

--- 4-express/app.js
+++ 4-express/app.js
@@ -5,9 +5,12 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+// Middleware
+app.use('/assets', express.static(__dirname + '/public'))
+
 // Responds to HTTP request with method get and map to URL '/' 
 app.get('/', (req, res) => {
-	res.send('<html><head></head><body><h1>Hello Nurse!</h1></body></html>')
+	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello Nurse!</h1></body></html>')
 })
 
 // : tells the JavaScript code in express that what follows could be anything
