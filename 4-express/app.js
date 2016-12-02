130ad718bdf59a005e8955f652f16c477b321496
Brian Doane <brianmdoane@gmail.com>
2016-12-02 13:46:38 -0600

    added routing to respond to http requests

--- 4-express/app.js
+++ 4-express/app.js
@@ -5,6 +5,22 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+// Responds to HTTP request with method get and map to URL '/' 
+app.get('/', (req, res) => {
+	res.send('<html><head></head><body><h1>Hello Nurse!</h1></body></html>')
+})
+
+// : tells the JavaScript code in express that what follows could be anything
+app.get('/employee/:id', (req, res) => {
+	res.send(`<html><head></head><body><h1>Employee: ${req.params.id}</h1></body></html>`)
+})
+
+// Get json data
+app.get('/api', (req, res) => {
+	// Convert JavaScript object to JSON string
+	res.json({ firstName: 'Montgomery', lastName: 'Burns', title: 'CEO' })
+})
+
 app.listen(port, () => {
 	console.log(`Listening on port: ${port}`)
 })
