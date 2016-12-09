0cab210fb3ba585eeea45a951f42fa4c5a66323d
Brian Doane <brianmdoane@gmail.com>
2016-12-08 21:43:10 -0600

    connected to mongodb with mlab

--- 6-mean-app/app.js
+++ 6-mean-app/app.js
@@ -1,14 +1,26 @@
 'use strict'
 
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const { connect } = require('./db/database')
 const app = express()
 
 const port = process.env.PORT || 3000
+app.set('port', port)
 
 app.use('/assets', express.static(__dirname + '/public'))
 
 app.set('view engine', 'ejs')
 
-app.listen(port, () => {
-	console.log(`Listening on port: ${port}`)
-})
+// Listen
+connect()
+	.then(() =>
+		app.listen(port, () => {
+			console.log(`Listening on port: ${port}`)
+		})
+	)
+	.catch(console.error)
+
+
+
